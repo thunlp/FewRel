ec1d862961265451d47c46e5ef8fc327b3af76dc
SillyXu <thu.hanxu13@gmail.com>
2022-05-04 22:38:50 +0800

    Add files via upload
    Add the script to download the pre-trained files for FewRel.

---
+++ download_pretrain.sh
@@ -0,0 +1,6 @@
+mkdir pretrain
+wget -P pretrain https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/glove/glove.6B.50d_mat.npy
+wget -P pretrain https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/glove/glove.6B.50d_word2id.json
+wget -P pretrain https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/config.json
+wget -P pretrain https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/pytorch_model.bin
+wget -P pretrain https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/vocab.txt
